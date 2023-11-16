a0eaff1e8e74d805a39a1b5ce52484bd8e02ba84
savedim <savedimdev@gmail.com>
2023-11-16 16:03:30 +0300

    Add counterdown before next action

--- App.js
+++ App.js
@@ -1,36 +1,77 @@
-import React, { useState, useEffect } from "react";
-import { TouchableOpacity, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from "react";
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-// TODO: api url https://collectapi.com/api/pray/pray-times-api
-
 const HomeScreen = () => {
-  const [countdown, setCountdown] = useState(300);
+  const [nextPrayer, setNextPrayer] = useState("");
+  const [countdown, setCountdown] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    fetchPrayerTimes(); // Запускаем запрос при загрузке компонента
+  }, []);
+
+  const validPrayers = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"];
+
+  // Функция для запроса времен молитв
+  const fetchPrayerTimes = useCallback(async () => {
+    try {
+      const today = new Date();
+      const formattedDate = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
+      const latitude = 36.5451; // Замените на вашу широту
+      const longitude = 31.9974; // Замените на вашу долготу
+
+      const response = await fetch(`https://api.aladhan.com/v1/timings/${formattedDate}?latitude=${latitude}&longitude=${longitude}&method=2`);
+      const data = await response.json();
+      const prayerTimes = data.data.timings;
+
+      const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      const nextPrayer = validPrayers.find(prayer => prayerTimes[prayer] > currentTime);
+
+      if (nextPrayer) {
+        const nextPrayerTime = prayerTimes[nextPrayer];
+        setNextPrayer(nextPrayer);
+        startCountdown(nextPrayerTime);
+      }
+    } catch (error) {
+      console.error("Ошибка загрузки данных:", error);
+    }
+  }, []);
+
+  const startCountdown = (prayerTime) => {
+    const now = new Date();
+    const [hours, minutes] = prayerTime.split(':');
+    const prayerDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes);
+    const timeDiff = prayerDate - now;
+    setCountdown(timeDiff > 0 ? Math.floor(timeDiff / 1000) : 0);
+
+    const countdownInterval = setInterval(() => {
       setCountdown((prevCountdown) => (prevCountdown > 0 ? prevCountdown - 1 : 0));
     }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+    // Остановка интервала и новый запрос после завершения обратного отсчета
+    setTimeout(() => {
+      clearInterval(countdownInterval);
+      fetchPrayerTimes(); // Запускаем новый запрос
+    }, timeDiff);
+
+    return () => clearInterval(countdownInterval);
+  };
 
   const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    return `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
   };
 
   return (
-      <ImageBackground
-          source={require('./assets/testphotos/background.jpg')}
-          style={styles.backgroundImage}
-      >
-        <View style={styles.container}>
-          <Text style={styles.countdownText}>{formatTime(countdown)} left to pray</Text>
-        </View>
-      </ImageBackground>
+    <ImageBackground
+      source={require('./assets/testphotos/background.jpg')}
+      style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Text style={styles.countdownText}>{formatTime(countdown)} before {nextPrayer}</Text>
+      </View>
+    </ImageBackground>
   );
 };
 
